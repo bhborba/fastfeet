409139a9664541d6696d61caf4e33253194f9d14
bhborba <brunohenriqueborba@gmail.com>
2020-04-06 14:59:03 -0300

    Rota inicial

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import {PersistGate} from 'redux-persist/integration/react';
 import {Provider} from 'react-redux';
 import {StatusBar} from 'react-native';
-import Routes from './routes';
 
 import './config/ReactotronConfig';
 import {store, persistor} from './store';
+import App from './App';
 
-export default function App() {
+export default function Index() {
     return (
         <Provider store={store}>
             <PersistGate persistor={persistor}>
                 <StatusBar barStyle="light-content" backgroundColor="#7D40E7" />
-                <Routes />
+                <App />
             </PersistGate>
         </Provider>
     );
--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,5 +1,23 @@
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 
 import SignIn from './pages/SignIn';
 
-export default createAppContainer(createSwitchNavigator({SignIn}));
+import Deliveries from './pages/Deliveries';
+
+export default (signedIn = false) =>
+    createAppContainer(
+        createSwitchNavigator(
+            {
+                Sign: createSwitchNavigator({
+                    SignIn,
+                }),
+                App: createBottomTabNavigator({
+                    Deliveries,
+                }),
+            },
+            {
+                initialRouteName: signedIn ? 'App' : 'Sign',
+            }
+        )
+    );
