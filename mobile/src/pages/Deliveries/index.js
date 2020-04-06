bfdfdee3b7f6be4f5ddc3f7ef8da4723cccb1031
bhborba <brunohenriqueborba@gmail.com>
2020-04-06 17:31:31 -0300

    parcial tela principal

--- mobile/src/pages/Deliveries/index.js
+++ mobile/src/pages/Deliveries/index.js
@@ -1,14 +1,76 @@
 import React from 'react';
-import {View} from 'react-native';
-
+import {StatusBar} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {
+    Container,
+    Header,
+    Avatar,
+    Info,
+    UserMessage,
+    Welcome,
+    UserName,
+    Content,
+    ContentHeader,
+    Deliver,
+    Filter,
+    Pending,
+    Delivered,
+    DeliveredText,
+    PendingText,
+    List,
+    ListContent,
+    PackageName,
+} from './styles';
 
 Icon.loadFont();
 
-// import { Container } from './styles';
-
 export default function Deliveries() {
-    return <View />;
+    return (
+        <Container>
+            <StatusBar barStyle="dark-content" backgroundColor="#6a51ae" />
+            <Header>
+                <Info>
+                    <Avatar
+                        source={{
+                            uri:
+                                'https://www.diariodocentrodomundo.com.br/wp-content/uploads/2018/08/bolsonaro-4.jpg',
+                        }}
+                    />
+                    <UserMessage>
+                        <Welcome>Bem vindo de volta,</Welcome>
+                        <UserName>Doidão do superpop</UserName>
+                    </UserMessage>
+                </Info>
+                <Icon name="exit-to-app" size={25} color="#E74040" />
+            </Header>
+
+            <Content>
+                <ContentHeader>
+                    <Deliver>Entregas</Deliver>
+                    <Filter>
+                        <Pending>
+                            <PendingText>Pendentes</PendingText>
+                        </Pending>
+                        <Delivered>
+                            <DeliveredText>Entregues</DeliveredText>
+                        </Delivered>
+                    </Filter>
+                </ContentHeader>
+
+                <List>
+                    <ListContent>
+                        <Icon name="local-shipping" size={25} color="#7D40E7" />
+                        <PackageName>Entrega</PackageName>
+                    </ListContent>
+                    <ListContent>
+                        <Icon name="local-shipping" size={25} color="#7D40E7" />
+                        <PackageName>Entrega</PackageName>
+                    </ListContent>
+                </List>
+            </Content>
+        </Container>
+    );
 }
 
 Deliveries.navigationOptions = {
--- mobile/src/pages/Deliveries/styles.js
+++ mobile/src/pages/Deliveries/styles.js
@@ -1,3 +1,89 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.SafeAreaView`
+    flex: 1;
+`;
+
+export const Header = styled.View`
+    margin-top: 30px;
+    align-items: center;
+    flex-direction: row;
+    padding: 0 20px;
+    display: flex;
+    justify-content: space-between;
+`;
+
+export const Info = styled.View`
+    flex-direction: row;
+    align-items: center;
+`;
+
+export const Avatar = styled.Image`
+    border-radius: 50px;
+    width: 68px;
+    height: 68px;
+`;
+
+export const UserMessage = styled.View`
+    flex-direction: column;
+    margin-left: 12px;
+`;
+
+export const Welcome = styled.Text`
+    color: #666666;
+    font-size: 12px;
+`;
+
+export const UserName = styled.Text`
+    color: #444444;
+    font-weight: bold;
+    font-size: 22px;
+`;
+
+export const Content = styled.View`
+    margin-top: 22.5px;
+    padding: 0 20px;
+`;
+
+export const ContentHeader = styled.View`
+    flex-direction: row;
+    display: flex;
+    justify-content: space-between;
+    align-items: baseline;
+`;
+
+export const Deliver = styled.Text`
+    color: #444444;
+    font-size: 22px;
+    font-weight: bold;
+`;
+
+export const Filter = styled.View`
+    flex-direction: row;
+    display: flex;
+    justify-content: space-between;
+`;
+
+export const Pending = styled.TouchableOpacity`
+    margin-right: 15px;
+`;
+
+export const PendingText = styled.Text``;
+
+export const Delivered = styled.TouchableOpacity``;
+
+export const DeliveredText = styled.Text``;
+
+export const List = styled.FlatList.attrs({
+    showsVerticalScrollIndicator: false,
+    contentContainerStyle: {padding: 30},
+})``;
+
+export const ListContent = styled.View`
+    margin-bottom: 15px;
+    box-shadow: #0000001a;
+`;
+
+export const PackageName = styled.Text`
+    font-size: 50px;
+`;
